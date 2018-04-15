209ca093f61ab915d6438cc78da19b3b343e3b6e
SzucsAti <atiszucs94@gmail.com>
2018-04-15 21:06:10 +0200

    development start

---
+++ app/src/main/java/com/example/attila/mobilszoftlab/interactor/BookInteractor.java
@@ -0,0 +1,21 @@
+package com.example.attila.mobilszoftlab.interactor;
+
+public class BookInteractor {
+
+    public BookInteractor() {
+
+    }
+
+    public void getBooks() {
+
+    }
+
+    public void insertBook() {
+
+    }
+
+    public void deleteBook() {
+
+    }
+}
+
